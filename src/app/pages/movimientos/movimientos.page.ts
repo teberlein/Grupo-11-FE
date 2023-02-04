e875b0041d4b2aef229b1644f46b276dc6640891
lucasbena <106684221+lucasbena@users.noreply.github.com>
2023-02-04 15:16:37 -0300

    boton borrar

--- src/app/pages/movimientos/movimientos.page.ts
+++ src/app/pages/movimientos/movimientos.page.ts
@@ -4,12 +4,16 @@ import { NavController } from '@ionic/angular';
 import { CuentasService } from 'src/app/core/services/cuentas.service';
 import { MovimientosService } from 'src/app/core/services/movimientos.service';
 
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-movimientos',
   templateUrl: './movimientos.page.html',
   styleUrls: ['./movimientos.page.scss'],
 })
 export class MovimientosPage implements OnInit {
+
+  result?: string;
 
   movimiento = {
     id:0,
@@ -26,6 +30,7 @@ export class MovimientosPage implements OnInit {
     private cuentasService: CuentasService,
     private movimientosService: MovimientosService,
     private navCtrl: NavController,
+    private actionSheetCtrl: ActionSheetController,
   ) {
     ar.params.subscribe(async param =>{
       console.log(param["id"]);
@@ -46,4 +51,31 @@ export class MovimientosPage implements OnInit {
     this.navCtrl.navigateBack("/vermas")
   }
 
-}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: '¿Estas seguro?',
+      subHeader: 'Se perdera el registro de esta información',
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+
+    const result = await actionSheet.onDidDismiss();
+    this.result = JSON.stringify(result, null, 2);
+
+}}
